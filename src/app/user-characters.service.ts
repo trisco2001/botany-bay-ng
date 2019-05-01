64701de5b9d5061a17453abd54ae42370d9a13f9
Tristan Leonard <tristanl@slalom.com>
2019-05-01 15:32:48 -0700

    adding header for cors

--- src/app/user-characters.service.ts
+++ src/app/user-characters.service.ts
@@ -29,24 +29,27 @@ interface UpdateUserCharactersBody {
 })
 export class UserCharactersService {
   configUrl = "https://zxxipdok10.execute-api.us-west-2.amazonaws.com/dev/userCharacters"
+  readonly options = {
+    headers: {'Content-Type': 'application/json'}
+  }
 
   constructor(private http: HttpClient) { }
 
   async getUserCharacters(userId: string) {
-    const classResponse = await this.http.get<UserCharactersResponse>(`${this.configUrl}/${userId}`).toPromise()
+    const classResponse = await this.http.get<UserCharactersResponse>(`${this.configUrl}/${userId}`, this.options).toPromise()
     return classResponse.Items
   }
 
   async updateUserCharacters(userId: string, id: string, characters: UserCharacter[]) {
     let body = JSON.stringify(characters)
-    const classResponse = await this.http.put<UserCharactersResponse>(`${this.configUrl}/${userId}/${id}`, body).toPromise()
+    const classResponse = await this.http.put<UserCharactersResponse>(`${this.configUrl}/${userId}/${id}`, body, this.options).toPromise()
     return classResponse.Items
   }
 
   async createUserCharacters(userId: string, characters: UserCharacter[]) {
     let object = { userId, characters }
     let body = JSON.stringify(object)
-    const classResponse = await this.http.post<UserCharactersResponse>(`${this.configUrl}`, body).toPromise()
+    const classResponse = await this.http.post<UserCharactersResponse>(`${this.configUrl}`, body, this.options).toPromise()
     return classResponse.Items
   }
 }
